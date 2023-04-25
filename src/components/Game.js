1030c0e62a38e53e0e04c19a53e7f482dfaa9232
lili7642 <lili7642@student.uu.se>
2023-04-25 22:55:16 +0200

    fixxx

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,10 +6,14 @@ import husdata from './hus/hus_data';
 /**
  * 
  * TODO:
- * -[] blablabla
- * -[] blablabla
- * -[] blablabla
- * -[] blablabla
+ * -[x] mindre bild som går att göra större
+ * -[] bilden hovrar över de andra elementen när zoomad
+ * -[] numrera bilderna på skärmen
+ * -[] skapa div för gjord gissning som visar om den var under eller över
+ * -[] lägg till statisk "SEK" inuti input field
+ * -[] visa ledtrådar
+ * -[] fyll ut databas
+ * -[] visa antal gissningar
  * 
  */
 
@@ -22,6 +26,7 @@ function Game(props) {
     const [bild, setBild] = useState(0);
     const [guess, setGuess] = useState("");
     const [stack, setStack] = useState([]);
+    const [isBig, setBig] = useState(false);
 
     const nästaBild = () => {
         bild < (hus.images.length - 1) ? setBild(bild + 1) : setBild(0);
@@ -29,6 +34,10 @@ function Game(props) {
 
     const föregåendeBild = () => {
         bild > 0 ? setBild(bild - 1) : setBild(hus.images.length - 1);
+    }
+
+    const toggleSize = () => {
+        setBig((prev) => !prev)
     }
 
     const handleSubmit = (event) => {
@@ -39,33 +48,43 @@ function Game(props) {
 
     const addDiv = () => {
         const thisGuess = guess;
-        const newDiv = 
+        if(thisGuess !== ""){
+            const newDiv = 
             <div key={stack.length}>
-                {thisGuess}
+                {thisGuess} SEK
             </div>;
-        setStack(prevStack => [...prevStack, newDiv]);
-        
+            setStack(prevStack => [newDiv, ...prevStack]);
+        }
     }
 
     return(
         <>
             <div className='imageContainer'>
-                <img src={hus.images[bild]} alt='Nuvarande bild'></img>
+                <img 
+                    className={isBig ? "image-large" : "image"} 
+                    src={hus.images[bild]} 
+                    alt='Nuvarande bild'
+                    onClick={toggleSize}
+                    ></img>
             </div>
 
             <div>
                 <button
                     type='button'
                     onClick={föregåendeBild}
-                >BACK</button>
+                >&#129092;</button>
 
                 <button
                     type='button'
                     onClick={nästaBild}
-                >NEXT</button>
+                >&#129094;</button>
             </div>
             
             <h1>{hus.adress}</h1>
+
+            <div>
+                {stack.map(div => div)} 
+            </div>
 
             <form onSubmit={handleSubmit}>
                 <input 
@@ -75,9 +94,6 @@ function Game(props) {
                 ></input>
                 <input type='submit'/>
             </form>
-            <div>
-                {stack.map(div => div)}
-            </div>
         </>
     );
 }
